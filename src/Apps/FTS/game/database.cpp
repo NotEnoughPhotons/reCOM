9f2b9ebc344d8249bc3d142c9bf5ebbf4f2a752d
adamdev <adamdevofficial@protonmail.com>
2025-10-22 23:51:12 -0400

    database: Cleaned up the "LoadWorld" function

--- src/Apps/FTS/game/database.cpp
+++ src/Apps/FTS/game/database.cpp
@@ -51,9 +51,9 @@ extern bool LoadWorld(const char* name)
 	appCamera = new CAppCamera(theWorld, theWorld->m_camera);
 	char pathbuf[64];
 		 
-	for (auto it = zdb::CAssetMgr::m_assets.begin(); it != zdb::CAssetMgr::m_assets.end(); ++it)
+	for (auto i = zdb::CAssetMgr::m_assets.begin(); i != zdb::CAssetMgr::m_assets.end(); ++i)
 	{
-		zdb::CAssetLib* library = *it;
+		zdb::CAssetLib* library = *i;
 
 		if (library->m_autoload)
 		{
@@ -63,87 +63,44 @@ extern bool LoadWorld(const char* name)
 			{
 				if (!library->m_models.empty())
 				{
-					s32 i = 0;
+					for (u32 j = 0; j != library->m_models.size(); j++)
+						hookupVisuals(&archive, library->m_models[j]);
 
-					do
-					{
-						hookupVisuals(&archive, library->m_models[i]);
-						i++;
-					}
-					while (i < library->m_models.size());
-
-					void* data = archive.ReleaseDataBuffer();
-					library->m_model_buffer = data;
+					library->m_model_buffer = archive.ReleaseDataBuffer();
 				}
 					
 				archive.Close(true);
 			}
 		}
-
-		archive.Close();
-
-		if (archive.m_filename)
-		{
-			zfree(archive.m_filename);
-		}
-
-		archive.m_filename = NULL;
-
-		theWorld->DismemberWorldModel();
-		theWorld->GenerateLandmarkList();
-		// theWorld->InheritRegionMasks(0, 0);
-		// theWorld->GenerateLightList();
 	}                                        
+
+	archive.Close();
+
+	if (archive.m_filename)
+		zfree(archive.m_filename);
+
+	archive.m_filename = NULL;
+
+	theWorld->DismemberWorldModel();
+	theWorld->GenerateLandmarkList();
+	theWorld->InheritRegionMasks(0, 0);
+	theWorld->GenerateLightList();
+
+	// TODO: Add lightmap texture code here
+	// End texture code
 
 	if (theWorld->m_numNoFarClipNodes == 0)
 	{
 		if (theWorld->m_camera->m_mid_plane < theWorld->m_camera->m_far_plane)
-		{
 			theWorld->m_camera->SetFarClip(theWorld->m_camera->m_mid_plane);
-		}
 	}
 
-	auto it = zdb::CAssetMgr::m_assets.begin();
-
-	while (it != zdb::CAssetMgr::m_assets.end())
-	{
-		zdb::CAssetLib* library = *it;
-
-		if (!library->m_autoload)
-		{
-			++it;
-			continue;
-		}
-		
-		sprintf_s(pathbuf, 64, "%s/%s/models.zar", gamez_GameRunPath, library->m_name);
-		
-		if (archive.Open(strbuf, -1, 1, 16))
-		{
-			if (!library->m_models.empty())
-			{
-				s32 i = 0;
-				
-				do
-				{
-					hookupVisuals(&archive, library->m_models[i++]);
-				}
-				while (i < library->m_models.size());
-			}
-
-			library->m_model_buffer = archive.ReleaseDataBuffer();
-		}
-		
-		++it;
-	}
-	
 	if (zdb::NodeUniverse)
-	{
 		zdb::NodeUniverse->m_locked = true;
-	}
 
 	thePipe.Flush();
-	
-	return false;
+
+	return true;
 }
 
 extern bool UnloadTheWorld()
