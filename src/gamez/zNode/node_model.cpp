ef7cb0dcf7b4dcf2dde3e7fb257baa9c0e278bef
adamdev <adamdevofficial@protonmail.com>
2025-10-22 23:53:30 -0400

    znode: Redo the definition of CModel::Read and CModel::Create

--- src/gamez/zNode/node_model.cpp
+++ src/gamez/zNode/node_model.cpp
@@ -2,6 +2,8 @@
 #include "node_saveload.h"
 #include "node_assetlib.h"
 
+#include <algorithm>
+
 namespace zdb
 {
 	CModel::CModel(const char* name)
@@ -18,16 +20,20 @@ namespace zdb
 	{
 		CModel* model = new CModel(NULL);
 
-		if (library && model)
-		{
+		if (library)
 			library->AddModel(model);
-		}
 
-		if (!model->Read(sload))
+		if (model->Read(sload))
+			return model;
+
+		if (library)
 		{
-			// TODO: Destroy the model after adding to the list. Node data is invalid.
+			auto target = std::find(
+				library->m_models.begin(),
+				library->m_models.end(),
+				model);
 
-			model = NULL;
+			library->m_models.erase(target);
 		}
 
 		return model;
@@ -35,9 +41,21 @@ namespace zdb
 
 	bool CModel::Read(CSaveLoad& sload)
 	{
-		zar::CKey* modelkey = sload.m_zfile.GetOpenKey();
-		if (modelkey != NULL)
+		tag_NODE_PARAMS nparams;
+
+		if (zar::CKey* modelkey = sload.m_zfile.GetOpenKey())
 		{
+			InitNodeParams(this);
+			
+			sload.m_zfile.Fetch("nparams", &nparams, sizeof(tag_NODE_PARAMS));
+			sload.m_zfile.Fetch("regionmask", &m_region_mask, sizeof(u32));
+
+			m_matrix = nparams.m_matrix;
+			m_bbox.m_min = nparams.m_bbox.m_min;
+			m_bbox.m_max = nparams.m_bbox.m_max;
+			m_type = nparams.m_type;
+			m_active = nparams.m_active;
+
 			CNode::Read(sload);
 			SetName(modelkey->GetName());
 		}
