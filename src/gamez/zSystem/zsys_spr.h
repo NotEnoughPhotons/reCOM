f08e046ca3a4e90162f29210ec644b46e57ab922
adamdev <adamdevofficial@protonmail.com>
2026-01-05 19:21:50 -0500

    zsystem: Create CSPR for ScratchPad memory access

---
+++ src/gamez/zSystem/zsys_spr.h
@@ -0,0 +1,10 @@
+#ifndef ZSYS_SPR_H
+#define ZSYS_SPR_H
+/// <summary>
+/// Class for dealing with the ScratchPad, a very-fast and efficient zone of memory.
+/// </summary>
+class CSPR
+{
+
+};
+#endif
