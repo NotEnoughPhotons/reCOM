7d1e6e72820c059cdd67b2b3d07bcbffa951e9db
adamdev <adamdevofficial@protonmail.com>
2026-01-06 15:19:39 -0500

    zsystem: Added Copy and Kick functions to CSPR

---
+++ src/gamez/zSystem/zsys_spr.cpp
@@ -0,0 +1,6 @@
+#include "zsys_spr.h"
+
+_word128* CSPR::Kick(_word128* packet, bool doKick)
+{
+	return NULL;
+}
--- src/gamez/zSystem/zsys_spr.h
+++ src/gamez/zSystem/zsys_spr.h
@@ -1,10 +1,14 @@
 #ifndef ZSYS_SPR_H
 #define ZSYS_SPR_H
+#include "zsys.h"
+
 /// <summary>
 /// Class for dealing with the ScratchPad, a very-fast and efficient zone of memory.
 /// </summary>
 class CSPR
 {
-
+public:
+	_word128* Copy(_word128* toSADR, _word128* toMADR, s32 toQWC);
+	_word128* Kick(_word128* packet, bool unknown1);
 };
 #endif
