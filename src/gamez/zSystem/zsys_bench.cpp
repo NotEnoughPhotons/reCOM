d7b023be8267e3fbd5e55708936c4e02cfe0b7ea
adamdev <adamdevofficial@protonmail.com>
2026-01-07 00:18:55 -0500

    zsystem: Added CBench for benchmarking performance

---
+++ src/gamez/zSystem/zsys_bench.cpp
@@ -0,0 +1,6 @@
+#include "zsys_bench.h"
+
+u32 CBench::countNodes = 0;
+u32 CBench::countRenderNodes = 0;
+u32 CBench::countVisuals = 0;
+f32 CBench::drawTimeX = 0.0f;
---
+++ src/gamez/zSystem/zsys_bench.h
@@ -0,0 +1,13 @@
+#ifndef ZSYS_BENCH_H
+#define ZSYS_BENCH_H
+#include "zsys.h"
+
+class CBench
+{
+public:
+	static u32 countNodes;
+	static u32 countRenderNodes;
+	static u32 countVisuals;
+	static f32 drawTimeX;
+};
+#endif
