4245febd485d4bb70c235fe71bcc0164b3ac81ce
adamdev <adamdevofficial@protonmail.com>
2026-01-07 00:27:04 -0500

    zsystem: Add new fields to CBench

--- src/gamez/zSystem/zsys_bench.cpp
+++ src/gamez/zSystem/zsys_bench.cpp
@@ -2,5 +2,8 @@
 
 u32 CBench::countNodes = 0;
 u32 CBench::countRenderNodes = 0;
+u32 CBench::countAlphaNodes = 0;
+u32 CBench::countClutterNodes = 0;
 u32 CBench::countVisuals = 0;
+u32 CBench::countAlphaVisuals = 0;
 f32 CBench::drawTimeX = 0.0f;
--- src/gamez/zSystem/zsys_bench.h
+++ src/gamez/zSystem/zsys_bench.h
@@ -5,9 +5,14 @@
 class CBench
 {
 public:
+	void Reset();
+
 	static u32 countNodes;
 	static u32 countRenderNodes;
+	static u32 countAlphaNodes;
+	static u32 countClutterNodes;
 	static u32 countVisuals;
+	static u32 countAlphaVisuals;
 	static f32 drawTimeX;
 };
 #endif
