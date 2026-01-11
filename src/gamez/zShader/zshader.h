863a69cf0cb895ebc19793c640a7e64ce36234f6
adamdev <adamdevofficial@protonmail.com>
2026-01-10 19:18:49 -0500

    zshader: Create new zShader module

--- src/gamez/zShader/zshader.h
+++ src/gamez/zShader/zshader.h
@@ -2,23 +2,62 @@
 #define ZSHADER_H
 #include "gamez/zSystem/zsys.h"
 
+#include "primitive.h"
+
+#include "vector"
+
+#include "glm/vec2.hpp"
+#include "glm/vec3.hpp"
+#include "glm/vec4.hpp"
+#include "glm/mat4x4.hpp"
+
+template<typename T>
+struct shaderuniform_t
+{
+    const char* name;
+    u32 location;
+    T type;
+};
+
 class CShader
 {
 public:
     CShader();
+    ~CShader();
 
-    void Init();
+    bool Create();
+    void Use();
+    void Destroy();
+
+    bool LoadVertexShader(const char*);
+    bool LoadFragmentShader(const char*);
+
+    void SetInt(const char*, s32);
+    void SetInt(const char*, u32);
+    void SetFloat(const char*, f32);
+    void SetVec2(const char*, glm::vec2);
+    void SetVec3(const char*, glm::vec3);
+    void SetVec4(const char*, glm::vec4);
+    void SetMat4(const char*, glm::mat4x4);
+    void SetTexture(const char*, u32);
+    void SetCubemap(const char*, u32);
+
+    u32 GetProgram() const { return m_program; }
+
+    u32 GetVertexShader() const { return m_vsHandle; }
+    u32 GetFragmentShader() const { return m_fsHandle; }
+private:
+    void CreateProgram();
+    void Attach();
     void Link();
-    
-    void Create(const char* path, u32& shader, const u32& type);
-    
-    void Bind();
-    void Unbind();
+    void InitUniforms();
 
-    static u32 m_program;
+    u32 m_program;
 
-    u32 m_ID;
-    u32 m_vertex;
-    u32 m_fragment;
+    const char* m_vsSource;
+    const char* m_fsSource;
+
+    u32 m_vsHandle;
+    u32 m_fsHandle;
 };
 #endif
--- src/gamez/zShader/zshader_main.cpp
+++ src/gamez/zShader/zshader_main.cpp
@@ -1,88 +1,191 @@
-#include <sstream>
 #include <fstream>
-
-#include <GL/glew.h>
+#include <sstream>
 
 #include "zshader.h"
 
+#include <GL\glew.h>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <SDL3/SDL.h>
+
 CShader::CShader()
 {
-    m_ID = 0;
-    
-    m_vertex = 0;
-    m_fragment = 0;
+    m_program = 0;
+    m_vsHandle = 0;
+    m_fsHandle = 0;
+
+    m_vsSource = nullptr;
+    m_fsSource = nullptr;
 }
 
-void CShader::Init()
+CShader::~CShader()
 {
-    m_ID = glCreateProgram();
+    Destroy();
+}
+
+bool CShader::Create()
+{
+    CreateProgram();
+    Attach();
+    Link();
+
+    return true;
+}
+
+void CShader::Use()
+{
+    glUseProgram(m_program);
+}
+
+void CShader::Destroy()
+{
+    glDeleteProgram(m_program);
+    glDeleteShader(m_vsHandle);
+    glDeleteShader(m_fsHandle);
+
+    m_program = 0;
+    m_vsHandle = 0;
+    m_fsHandle = 0;
+
+    delete m_vsSource;
+    m_vsSource = nullptr;
+    delete m_fsSource;
+    m_fsSource = nullptr;
+}
+
+bool CShader::LoadVertexShader(const char* path)
+{
+    std::fstream file(path);
+
+    if (!file.good())
+        return false;
+
+    std::stringstream buf;
+
+    buf << file.rdbuf();
+
+    m_vsSource = _strdup(buf.str().c_str());
+
+    m_vsHandle = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(m_vsHandle, 1, &m_vsSource, nullptr);
+    glCompileShader(m_vsHandle);
+
+    s32 success = 0;
+    glGetShaderiv(m_vsHandle, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        char infoLog[512];
+        glGetShaderInfoLog(m_vsHandle, 512, nullptr, infoLog);
+        SDL_Log("[ERROR] - Vertex error: %s", infoLog);
+    }
+
+    return true;
+}
+
+bool CShader::LoadFragmentShader(const char* path)
+{
+    std::fstream file(path);
+
+    if (!file.good())
+        return false;
+
+    std::stringstream buf;
+
+    buf << file.rdbuf();
+
+    m_fsSource = _strdup(buf.str().c_str());
+
+    m_fsHandle = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(m_fsHandle, 1, &m_fsSource, nullptr);
+    glCompileShader(m_fsHandle);
+
+    s32 success = 0;
+    glGetShaderiv(m_fsHandle, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        char infoLog[512];
+        glGetShaderInfoLog(m_fsHandle, 512, nullptr, infoLog);
+        SDL_Log("[ERROR] - Fragment error: %s", infoLog);
+    }
+
+    return true;
+}
+
+// TODO: Optimize these so we don't have to keep retrieving -
+// uniforms from the shaders every time.
+// Ideally we'd have a way to automatically get a list of uniforms,
+// generated at the time the program starts.
+// From there, we could just access the list whenever we want a uniform.
+
+void CShader::SetFloat(const char* name, f32 value)
+{
+    s32 location = glGetUniformLocation(m_program, name);
+    glUniform1f(location, value);
+}
+
+void CShader::SetVec2(const char* name, glm::vec2 value)
+{
+    s32 location = glGetUniformLocation(m_program, name);
+    glUniform2f(location, value.x, value.y);
+}
+
+void CShader::SetVec3(const char* name, glm::vec3 value)
+{
+    s32 location = glGetUniformLocation(m_program, name);
+    glUniform3f(location, value.x, value.y, value.z);
+}
+
+void CShader::SetVec4(const char* name, glm::vec4 value)
+{
+    s32 location = glGetUniformLocation(m_program, name);
+    glUniform4f(location, value.x, value.y, value.z, value.w);
+}
+
+void CShader::SetMat4(const char* name, glm::mat4x4 value)
+{
+    s32 location = glGetUniformLocation(m_program, name);
+    glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(value));
+}
+
+void CShader::SetTexture(const char* name, u32 texture)
+{
+    s32 location = glGetUniformLocation(m_program, name);
+    glUniform1i(location, texture);
+}
+
+void CShader::SetCubemap(const char* name, u32 cubemap)
+{
+    s32 location = glGetUniformLocation(m_program, name);
+    glUniform1i(location, cubemap);
+}
+
+void CShader::CreateProgram()
+{
+    m_program = glCreateProgram();
+}
+
+void CShader::Attach()
+{
+    if (m_program == 0)
+    {
+        return;
+    }
+
+    glAttachShader(m_program, m_vsHandle);
+    glAttachShader(m_program, m_fsHandle);
 }
 
 void CShader::Link()
 {
-    s32 success = false;
-    char infoLog[512];
-    
-    glAttachShader(m_ID, m_vertex);
-    glAttachShader(m_ID, m_fragment);
-    glLinkProgram(m_ID);
-    
-    glGetProgramiv(m_ID, GL_LINK_STATUS, &success);
-    if (!success)
-    {
-        glGetProgramInfoLog(m_ID, 512, NULL, infoLog);
-        printf_s("E R R O R : Shader linking failed!\n%s", infoLog);
-    }
-
-    glDeleteShader(m_vertex);
-    glDeleteShader(m_fragment);
-}
-
-void CShader::Create(const char* path, u32& shader, const u32& type)
-{
-    char* source = NULL;
-    
-    if (!path)
+    if (m_program == 0)
     {
         return;
     }
 
-    std::fstream io(path);
-
-    if (io.good())
-    {
-        std::stringstream buffer;
-
-        buffer << io.rdbuf();
-        io.close();
-        source = zstrdup(buffer.str().c_str());
-        buffer.flush();
-    }
-    else
-    {
-        return;
-    }
-
-    shader = glCreateShader(type);
-    glShaderSource(shader, 1, &source, NULL);
-    glCompileShader(shader);
-
-    s32 success;
-    char infoLog[512];
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(shader, 512, NULL, infoLog);
-        printf_s("E R R O R (%s) : Shader compilation failed!\n%s", path, infoLog);
-    }
+    glLinkProgram(m_program);
 }
 
-void CShader::Bind()
+void CShader::InitUniforms()
 {
-    glUseProgram(m_ID);
-}
 
-void CShader::Unbind()
-{
-    
 }
